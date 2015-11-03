dc5ab17a79237a17d16e3605ec69d5ac0cd52080
Romain Pouclet <romain.pouclet@gmail.com>
2015-11-02 19:04:19 -0500

    Setup crashlytics & Fabric

--- Rewatch/AppDelegate.swift
+++ Rewatch/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import KeychainSwift
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var client: Client!
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics.self])
+
         // Setup stylesheet
         let stylesheet = Stylesheet()
         stylesheet.apply()
