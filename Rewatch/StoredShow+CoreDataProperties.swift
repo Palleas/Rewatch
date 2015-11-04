aa4319ff92f94b88a6c985995b229a6dc4b6792a
Romain Pouclet <romain.pouclet@gmail.com>
2015-11-03 22:14:27 -0500

    Setup the show entity

---
+++ Rewatch/StoredShow+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  StoredShow+CoreDataProperties.swift
+//  Rewatch
+//
+//  Created by Romain Pouclet on 2015-11-03.
+//  Copyright © 2015 Perfectly-Cooked. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension StoredShow {
+
+    @NSManaged var id: NSNumber?
+    @NSManaged var name: String?
+
+}
---
+++ Rewatch/StoredShow.swift
@@ -0,0 +1,17 @@
+//
+//  StoredShow.swift
+//  Rewatch
+//
+//  Created by Romain Pouclet on 2015-11-03.
+//  Copyright © 2015 Perfectly-Cooked. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class StoredShow: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
