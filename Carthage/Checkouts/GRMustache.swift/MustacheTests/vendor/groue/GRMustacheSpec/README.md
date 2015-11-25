d24ebafd37f89b3c61532abaa25e32ddb7b710fb
Romain Pouclet <romain.pouclet@gmail.com>
2015-11-24 23:16:09 -0500

    Remove submodules

--- Carthage/Checkouts/GRMustache.swift/MustacheTests/vendor/groue/GRMustacheSpec/README.md
+++
@@ -1,3 +0,0 @@
-# GRMustacheSpec
-
-A test suite shared between the Objective-C [GRMustache](https://github.com/groue/GRMustache) and [GRMustache.swift](https://github.com/groue/GRMustache.swift).
--- Carthage/Checkouts/GRMustache.swift/Playground.playground/section-1.swift
+++
@@ -1,8 +0,0 @@
-// To run this playground, select and build the MustacheOSX scheme.
-
-import Mustache
-
-// Renders "Hello Arthur"
-let template = try! Template(string: "Hello {{ name }}")
-let rendering = try! template.render(Box(["name": "Arthur"]))
-print(rendering)
